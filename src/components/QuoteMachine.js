4f33642bbc86b5c4723d4eebe41a57e6addbf146
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2019-11-23 17:52:16 -0500

    Cleaned up code

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -2,20 +2,19 @@ import React from 'react';
 import Button from './Button';
 import Quote from './Quote';
 import Author from './Author';
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-
 
 
 
 const QuoteMachine = (props) => (
     <div id="quote-machine">
-        <div id="quote-box">{/* This is the old framework. Will work if uncommented  */}
-
+        <div id="quote-box">
+            
+            {/* This is the old framework. Will work if uncommented  */}
             {/* {props.finalQuote ? 
 props.finalQuote.quote 
 + "-" + props.finalQuote.author  : ""}
 <Button buttonDisplayName="Next Quote" clickHandler={props.assignNewQuoteIndex} /> */}
+           
             <Quote finalQuote={props.finalQuote}></Quote>
             <Author finalQuote={props.finalQuote}></Author>
             <Button buttonDisplayName="Next Quote" clickHandler={props.assignNewQuoteIndex} />
