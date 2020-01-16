9ce512cff08288da9b28fc60026c55c6a3dabdce
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2020-01-15 21:05:15 -0500

    Updates

---
+++ precache-manifest.80498ff2d98d6e2dd0f0e0deea36f52f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a76ebedd82585a4bf9fdb9e8949478",
+    "url": "/final-random-quote-machine/index.html"
+  },
+  {
+    "revision": "b43fc21db18b4875f189",
+    "url": "/final-random-quote-machine/static/css/main.62bfbaa9.chunk.css"
+  },
+  {
+    "revision": "a2d7519a48082111b053",
+    "url": "/final-random-quote-machine/static/js/2.d3dee208.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/final-random-quote-machine/static/js/2.d3dee208.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b43fc21db18b4875f189",
+    "url": "/final-random-quote-machine/static/js/main.ddc5283b.chunk.js"
+  },
+  {
+    "revision": "f06363a38eca028f371a",
+    "url": "/final-random-quote-machine/static/js/runtime-main.291c0e99.js"
+  }
+]);
